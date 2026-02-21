b2fc58ccff9eb33d68ed56f0d75e78a88595d420
Atulsaini2806 <atulsaini10123@gamil.com>
2026-02-21 22:32:46 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -2,17 +2,21 @@ const express = require('express');
 
 const app = express();
 
-app.use("/app", (req, res) => {
-    res.send('Hello from dashborad');
-});
 
 app.use("/hello", (req, res) => {
     res.send('Hello hello hello!');
+});
+app.use("/hello/2", (req, res) => {
+    res.send('Hello hello !');
 });
 
 app.use("/test", (req, res) => {
     res.send('Hello from the server!');
 });
+app.use("/", (req, res) => {
+    res.send('Atul Saini');
+});
+
   
 app.listen(7777, () => {
     console.log('Server is successfully listening on port 7777');

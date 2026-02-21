9c3bbfc3be3323568d8f9479dd817a70e668ca99
Atulsaini2806 <atulsaini10123@gamil.com>
2026-02-22 00:27:49 +0530

    advanced

--- src/app.js
+++ src/app.js
@@ -2,17 +2,19 @@ const express = require('express');
 
 const app = express();
 
-app.get("/user" , (req ,res) => {
+app.get("/user/:userId/:name/:passward" , (req ,res) => {
+    console.log(req.params);
+    
   res.send({firstname:"Atul ", lastname:"Saini"});
 });
 
-app.post("/user" , (req ,res) => {
-  res.send("successfully posted data to the database.");
-});
+// app.post("/user" , (req ,res) => {
+//   res.send("successfully posted data to the database.");
+// });
 
-app.delete("/user" , (req ,res) => {
-  res.send("successfully deleted data from the database.");
-});
+// app.delete("/user" , (req ,res) => {
+//   res.send("successfully deleted data from the database.");
+// });
 
 app.use("/test", (req, res) => {
     res.send('Hello from the server!');

9f19fe34a4edc183b4bd4eff2732850319798900
Atulsaini2806 <atulsaini10123@gamil.com>
2026-02-22 23:09:52 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -2,16 +2,14 @@ const express = require('express');
 const app = express();
 
 app.use("/user", (req ,res,next) => {
+    res.send("Route handler two");
+});
+app.use("/user", (req ,res,next) => {
     res.send("Route handler one");
    next();
-       //res.send("Route handler one");
-},
-(req,res) => {
-  res.send("Route handler 2");
-}
-);
+});
 
 
 app.listen(7777, () => {
-    console.log('Server is successfully listening on port 7777');
+    console.log('Server is successfully lis   tening on port 7777');
 });

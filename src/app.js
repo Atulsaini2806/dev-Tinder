2d5bef258d2b14a827b7b0ba18663b2ec5b9a913
Atulsaini2806 <atulsaini10123@gamil.com>
2026-02-22 12:41:07 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -1,24 +1,17 @@
 const express = require('express');
 const app = express();
 
-app.get("/user/:userId/:name/:passward" , (req ,res) => {
-    console.log(req.params);
-    
-  res.send({firstname:"Atul ", lastname:"Saini"});
-});
+app.use("/user", (req ,res,next) => {
+    res.send("Route handler one");
+   next();
+       //res.send("Route handler one");
+},
+(req,res) => {
+  res.send("Route handler 2");
+}
+);
 
-// app.post("/user" , (req ,res) => {
-//   res.send("successfully posted data to the database.");
-// });
 
-// app.delete("/user" , (req ,res) => {
-//   res.send("successfully deleted data from the database.");
-// });
-
-app.use("/test", (req, res) => {
-    res.send('Hello from the server!');
-});
-  
 app.listen(7777, () => {
     console.log('Server is successfully listening on port 7777');
 });

958fb6c03d3bd69bf10c56e73b6ffb1414563be8
Atulsaini2806 <atulsaini10123@gamil.com>
2026-02-23 01:35:29 +0530

    exploreing more

--- src/app.js
+++ src/app.js
@@ -1,15 +1,35 @@
 const express = require('express');
+const connectDB = require('./config/database');
 const app = express();
+const User = require('./models/user');
 
-app.use("/user", (req ,res,next) => {
-    res.send("Route handler two");
-});
-app.use("/user", (req ,res,next) => {
-    res.send("Route handler one");
-   next();
+app.post("/signup", async (req, res) => {
+    const user = new User ({
+        firstName: "Virat",
+        lastName: "Kohil",
+        emailId: "virat@123",
+        passward: "65432112345" 
+    });
+
+    try{
+     await user.save();
+    res.send("user added successfully");
+    }
+    catch(err){
+        res.status(400).send("Error in adding user: " + err.message);
+    }
 });
 
 
-app.listen(7777, () => {
-    console.log('Server is successfully lis   tening on port 7777');
+connectDB().then(() => {
+    console.log("Database connected successfully.....");
+    app.listen(7777, () => {
+    console.log('Server is successfully listening on port 7777');
+  });
+})
+.catch((err) => {
+    console.error("Database connection failed.....");
 });
+
+
+

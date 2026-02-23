3624b785f5eda348fce01824c899d08e6af8b87a
Atulsaini2806 <atulsaini10123@gamil.com>
2026-02-23 22:09:49 +0530

    get API's

--- src/app.js
+++ src/app.js
@@ -18,6 +18,38 @@ app.post("/signup", async (req, res) => {
     }
  });
 
+ //Get user by mail
+app.get("/user", async (req, res) => {
+    const userEmail = req.body.emailId;
+  try{
+    const user = await User.findOne({emailId : userEmail});
+    if(!user){
+        return res.status(404).send("User not found with email: " + userEmail);
+    } else{
+        res.send(user);
+    }
+    // const users = await User.find({emailId : userEmail});
+    // if(users.length === 0){
+    //     return res.status(404).send("User not found with email: " + userEmail);
+    // } else{
+    //     res.send(users);
+    // }
+  }
+  catch(err){
+    res.status(400).send("something went wrong: " + err.message);
+  }
+});
+
+ // feed API - GET /feed - get all the user from the database
+app.get("/feed", async (req, res) => {
+    try{
+        const users = await User.find({});
+        res.send(users);
+    }
+    catch(err){
+        res.status(400).send("Error in fetching users: " + err.message);
+    }
+});
 
 connectDB().then(() => {
     console.log("Database connected successfully.....");

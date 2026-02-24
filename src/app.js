10c3899b14ef8ba6014335e062aff1c716ec5c5f
Atulsaini2806 <atulsaini10123@gamil.com>
2026-02-24 19:05:01 +0530

    Get,Post,Patch,Delete API's call

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const connectDB = require('./config/database');
 const app = express();
 const User = require('./models/user');
 
-app.use(express.json());
+app.use(express.json()); 
 
 app.post("/signup", async (req, res) => { 
     //creating a new instance of the user model

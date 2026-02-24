74ebd871baf4f08e2fb76c8c5cc114eb5bb169e3
Atulsaini2806 <atulsaini10123@gamil.com>
2026-02-24 19:25:31 +0530

    data validation

--- src/models/user.js
+++ src/models/user.js
@@ -2,22 +2,41 @@ const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
     firstName:{
-        type : String
+        type : String,
+        required : true,
+        minLength : 4,
+        maxLength : 20
     },
     lastName:{
         type : String
     },
     emailId:{
-        type : String
+        type : String,
+        required : true,
+        unique : true,
+        lowercase : true,
+        trim : true
     },
     passward:{ 
         type : String
     },
     age:{
-        type : Number
+        type : Number,
+        min : 18
     },
     gender:{
         type : String
+    },
+    photoUrl:{
+        type : String,
+        default : "https://www.pngitem.com/middle/ixwxbwh_user-profile-dummy-hd-png-download/"
+    },
+    about:{
+        type : String,
+        default : "This is the default about the user!"
+    },
+    skills:{
+        type : [String]
     }
 });
 

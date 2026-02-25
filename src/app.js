a8da7800ade32b215d9b516a5db4c1a0a5f13d8c
Atulsaini2806 <atulsaini10123@gamil.com>
2026-02-25 13:55:42 +0530

    API level sanitization

--- src/app.js
+++ src/app.js
@@ -65,10 +65,24 @@ app.delete("/user", async (req, res) => {
 });
 
 //update the data of the user
-app.patch("/user", async (req, res) => {
-    const userId = req.body.userId;
+app.patch("/user/:userId", async (req, res) => {
+    const userId = req.params?.userId;
    const data = req.body;
+
    try{
+     const ALLOWED_UPDATES = ["userId","photoUrl", "about", "gender", "age","skills"];
+
+   const isUpdateAllowed = Object.keys(data).every((k) => 
+    ALLOWED_UPDATES.includes(k));
+
+   if(!isUpdateAllowed){
+    throw new error("Update not allowed");
+   }
+
+   if(data?.skills.length>10){
+    throw new error("Skills should not be more than 10");
+   }
+
     const user = await User.findByIdAndUpdate( {_id: userId}, data, {runValidators : true});
     res.send("User updated successfully");
    }

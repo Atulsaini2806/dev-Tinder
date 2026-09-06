8d86b406e66e98994fb8074565eca686b7c4de71
Atulsaini2806 <atulsaini10123@gamil.com>
2026-09-06 23:48:17 +0530

    Editing

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     await mongoose.connect(
-        "mongodb+srv://atul_saini:8c6kad6EAyEpnY31@atulsaini-cluster.g4sc5jb.mongodb.net/devTinder"
+        "mongodb+srv://atul_saini:g0yCt8xTpexyjOcL@atulsaini-cluster.g4sc5jb.mongodb.net/"
     );
 };
 
